bfa3e6808827d0c934bc1d8849218e945794dfa0
Mehnazahmed <mehnazchowdhury613@gmail.com>
2022-11-21 17:49:57 +0600

    ade

--- src/Pages/DashBoard/Payment/CheckOutForm.js
+++ src/Pages/DashBoard/Payment/CheckOutForm.js
@@ -31,27 +31,30 @@ const CheckOutForm = () => {
 
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-sm mt-4 btn-primary' type="submit" >
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className='btn btn-sm mt-4 btn-primary' type="submit" >
+                    Pay
+                </button>
+            </form>
+            <p className='text-red-500'>{cardError}</p>
+        </>
     );
 };
 
